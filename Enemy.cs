c6041b8642b7d050fc6774c3519fc99fa38cae25
JeppeKjeldal <jeppekjeldal95@gmail.com>
2023-11-28 13:22:14 +0100

    Klasser og objekter

---
+++ Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semester1Projekt
+{
+    internal class Enemy : GameObject
+    {
+        private int health;
+        private int damage;
+        private Vector2 enemyPosition;
+        private Vector2 checkpointTarget;
+        private float enemyMovementSpeed;
+        
+        //Get og set på health
+
+        public Enemy(int health, Vector2 enemyPosition, float enemyMovementSpeed, int damage)
+        {
+            this.health = health;
+            this.damage = damage;
+            this.enemyPosition = enemyPosition;
+            this.enemyMovementSpeed = enemyMovementSpeed;
+        }
+
+        public void Pathing()// Mangler parametre
+        {
+
+        }
+
+        public void DamageNexus()// Mangler parametre
+        {
+
+        }
+    }
+}
