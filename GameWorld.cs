f178e480d9dbed6dd2a46263058e4e9362137d6f
axpterrax1 <axp_terrax@hotmail.com>
2023-11-29 10:58:46 +0100

    targeting

--- GameWorld.cs
+++ GameWorld.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Semester1Projekt
 {
@@ -8,6 +9,10 @@ namespace Semester1Projekt
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        public static float DeltaTime { get; private set; }
+        private static List<GameObject> gameObjects;
+        private Tower tower;
+        private Enemy[] enemy;
 
         public GameWorld()
         {
@@ -19,6 +24,18 @@ namespace Semester1Projekt
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            gameObjects = new List<GameObject>();
+            tower = new Tower();
+            gameObjects.Add(tower);
+
+            enemy = new Enemy[3];
+            enemy[0] = new Enemy();
+            enemy[1] = new Enemy();
+            enemy[2] = new Enemy();
+            gameObjects.Add(enemy[0]);
+            gameObjects.Add(enemy[1]);
+            gameObjects.Add(enemy[2]);
+
 
             base.Initialize();
         }
@@ -36,6 +53,16 @@ namespace Semester1Projekt
                 Exit();
 
             // TODO: Add your update logic here
+            DeltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            foreach (GameObject gameObject in gameObjects)
+            {
+                tower.Update();
+                //enemy.Update();
+                
+            }
+
+            tower.ScanForEnemies(enemy);
 
             base.Update(gameTime);
         }
